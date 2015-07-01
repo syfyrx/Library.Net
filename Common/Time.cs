670ed1b6a5449d508992108b851c9d99f7619913
syfyrx <522733724@qq.com>
2015-07-01 15:16:24 +0800

    add time lib

---
+++ Common/Time.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library.Net.Common
+{
+    public class Time
+    {
+        /// <summary> 
+        /// 获取时间戳 
+        /// </summary> 
+        /// <returns></returns> 
+        public static string GetTimeStamp()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalMilliseconds).ToString();
+        }
+    }
+}
