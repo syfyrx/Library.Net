8f9b603846073f0f31cc8da787cab87099ba33e9
syfyrx <522733724@qq.com>
2015-08-22 11:07:57 +0800

    update AppConfig.cs

--- Common/AppConfig.cs
+++ Common/AppConfig.cs
@@ -6,73 +6,94 @@ using System.Configuration;
 
 namespace Library.Net.Common
 {
-	public static class AppConfig
-	{
-		/// <summary>
-		/// 设置配置文件AppSettings节点的某项值
-		/// </summary>
-		/// <param name="key">Key</param>
-		/// <param name="value">Value</param>
-		public static void SetAppSettings(string key, string value)
-		{
-			try
-			{
-				Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-				if (config.AppSettings.Settings[key] != null)
-					config.AppSettings.Settings[key].Value = value;
-				else
-					config.AppSettings.Settings.Add(key, value);
-				config.Save(ConfigurationSaveMode.Modified);
-				ConfigurationManager.RefreshSection("appSettings");
-			}
-			catch (Exception ex)
-			{
-				throw ex;
-			}
-		}
-		/// <summary>
-		/// 读取配置文件AppSettings节点的某项值
-		/// </summary>
-		/// <param name="key">key</param>
-		/// <returns></returns>
-		public static string GetAppSettings(string key)
-		{
-			string result = "";
-			try
-			{
-				Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-				if (config.AppSettings.Settings[key] != null)
-				{
-					result = config.AppSettings.Settings[key].Value;
-				}
-			}
-			catch (Exception ex)
-			{
-				throw ex;
-			}
-			return result;
-		}
-		/// <summary>
-		/// 读取配置文件ConnectionStrings节点的某项值
-		/// </summary>
-		/// <param name="name">name</param>
-		/// <returns></returns>
-		public static string GetConnectionStrings(string name)
-		{
-			string result = "";
-			try
-			{
-				Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-				if (config.ConnectionStrings.ConnectionStrings[name] != null)
-				{
-					result = config.ConnectionStrings.ConnectionStrings[name].ConnectionString;
-				}
-			}
-			catch (Exception ex)
-			{
-				throw ex;
-			}
-			return result;
-		}
-	}
+    public static class AppConfig
+    {
+        /// <summary>
+        /// 读取配置文件AppSettings节点的某项值
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns></returns>
+        public static string GetAppSettings(string key)
+        {
+            string res = null;
+            foreach (string strKey in ConfigurationManager.AppSettings)
+            {
+                if (strKey == key)
+                {
+                    res = ConfigurationManager.AppSettings[key];
+                    break;
+                }
+            }
+            return res;
+        }
+        /// <summary>
+        /// 设置配置文件AppSettings节点的某项值
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value</param>
+        public static void SetAppSettings(string key, string value)
+        {
+            bool isModified = false;
+            foreach (string strKey in ConfigurationManager.AppSettings)
+            {
+                if (strKey == key)
+                {
+                    isModified = true;
+                }
+            }
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (isModified)
+            {
+                config.AppSettings.Settings.Remove(key);
+            }
+            config.AppSettings.Settings.Add(key, value);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        /// <summary>
+        /// 读取配置文件ConnectionStrings节点的某项值
+        /// </summary>
+        /// <param name="name">name</param>
+        /// <returns></returns>
+        public static string GetConnectionStrings(string name)
+        {
+            string res = null;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.ConnectionStrings.ConnectionStrings[name] != null)
+            {
+                res = config.ConnectionStrings.ConnectionStrings[name].ConnectionString;
+            }
+            return res;
+        }
+        ///<summary> 
+        ///设置配置文件ConnectionStrings节点的某项值
+        ///</summary> 
+        ///<param name="name">连接字符串名称</param> 
+        ///<param name="connectionString">连接字符串内容</param> 
+        ///<param name="providerName">数据提供程序名称</param> 
+        private static void SetConnectionStrings(string name, string connectionString, string providerName)
+        {
+            bool isModified = false;
+            if (ConfigurationManager.ConnectionStrings[name] != null)
+            {
+                isModified = true;
+            }
+            //新建一个连接字符串实例
+            ConnectionStringSettings newSettings = new ConnectionStringSettings(name, connectionString, providerName);
+            // 打开可执行的配置文件*.exe.config
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            // 如果连接串已存在，首先删除它
+            if (isModified)
+            {
+                config.ConnectionStrings.ConnectionStrings.Remove(name);
+            }
+            // 将新的连接串添加到配置文件中.  
+            config.ConnectionStrings.ConnectionStrings.Add(newSettings);
+            // 保存对配置文件所作的更改  
+            config.Save(ConfigurationSaveMode.Modified);
+            // 强制重新载入配置文件的ConnectionStrings配置节  
+            ConfigurationManager.RefreshSection("ConnectionStrings");
+        }
+    }
 }
