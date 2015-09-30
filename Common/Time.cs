315864c9612a68e4e785653cbdd59e7766cef92e
syfyrx <522733724@qq.com>
2015-09-30 10:09:03 +0800

    添加时间和时间戳转换

--- Common/Time.cs
+++ Common/Time.cs
@@ -16,5 +16,27 @@ namespace Library.Net.Common
             TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalMilliseconds).ToString();
         }
+        /// <summary>
+        /// Unix时间戳格式转为DateTime时间格式
+        /// </summary>
+        /// <param name=”timeStamp”></param>
+        /// <returns></returns>
+        private DateTime GetTime(string timeStamp)
+        {
+            DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long lTime = long.Parse(timeStamp + "0000000");
+            TimeSpan toNow = new TimeSpan(lTime);
+            return dtStart.Add(toNow);
+        }
+        /// <summary>
+        /// DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        /// <param name=”time”></param>
+        /// <returns></returns>
+        private int ConvertDateTimeInt(System.DateTime time)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
     }
 }
