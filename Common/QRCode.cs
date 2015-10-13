c84e8b5259e79f7ecef89e705c3ad5fcb7cb300a
syfyrx <522733724@qq.com>
2015-10-13 17:58:01 +0800

    添加二维码生成

---
+++ Common/QRCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using ThoughtWorks.QRCode.Codec;
+
+namespace Library.Net.Common
+{
+    /// <summary>
+    /// 二维码
+    /// </summary>
+    public static class QRCode
+    {
+        public static Bitmap Generate(string txt)
+        {
+            Bitmap res;
+            string enCodeString = txt;
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            res = qrCodeEncoder.Encode(enCodeString, Encoding.UTF8);
+            return res;
+        }
+    }
+}
